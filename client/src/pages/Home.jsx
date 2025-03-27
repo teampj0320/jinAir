b8f24bc05176a90e3a39179cf719a68f8bb6f10b
haon <haoony@naver.com>
2025-03-27 10:22:18 +0900

    하온 Home 컴포넌트 수정

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,16 @@ import MainSearch from '../component/main/MainSearch.jsx';
 
 export default function Home() {
     return (
-        <div className='content'>
-            <MainSearch />
-            <span>
-                여긴 메인 홈이지롱
-            </span>
-        </div>
+        <>
+            <div>
+                <MainSearch />
+            </div>
+            <div className='content'>
+                <span>
+                    여긴 메인 홈이지롱
+                </span>
+            </div>
+        </>
     );
 }
 
