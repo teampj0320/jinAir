bde5aee1a49aad0873d7c4ce7cf9b86e49f94f17
haon <haoony@naver.com>
2025-03-27 18:22:39 +0900

    하온메인탑

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,13 +4,10 @@ import MainSearch from '../component/main/MainSearch.jsx';
 export default function Home() {
     return (
         <>
-            <div>
-                <MainSearch />
-            </div>
-            <div className='content'>
-                <span>
-                    여긴 메인 홈이지롱
-                </span>
+            <div className='content'> 
+                <div>
+                    <MainSearch />
+                </div>
             </div>
         </>
     );
