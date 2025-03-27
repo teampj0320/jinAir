0002ccafa7bc9931ff3232c0d111719fac1d0ec3
haon <haoony@naver.com>
2025-03-27 10:04:35 +0900

    컴포넌트 추가

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import MainSearch from '../component/main/MainSearch.jsx';
 
 export default function Home() {
     return (
         <div className='content'>
+            <MainSearch />
             <span>
                 여긴 메인 홈이지롱
             </span>
