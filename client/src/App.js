16b09f36bed7c1945dd4fe904c7529895d959761
2eo2yeo <2eo2yeo@gmail.com>
2025-03-27 19:16:22 +0900

    mypage ui 작업 중

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './scss/common.scss';
 import Layout from './pages/Layout.jsx';
 import Home from './pages/Home.jsx';
+import Mypage from './pages/Mypage.jsx';
 
-/* 대충 넣어놨으니까 알아서 수정하기~~~~~~~ */
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
+        <Route path='/mypage' element={<Mypage />} />
         {/* <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/mypage' element={<Mypage />} />
         <Route path='/payment' element={<Payment />} />
         <Route path='/admin' element={<Admin />} /> */}
       </Route>
--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import '../scss/ryeong.scss';
 
 export default function Mypage() {
     return (
-        <div className='r-common'>
-            
+        <div className='r-common mp-container'>
+            <div className='mp-content'>
+                <section className='mypage-subtitle box-style'>
+                    <p className='f30'>마이페이지</p>
+                </section>
+                <div className='mypage-index'>
+                    <section className='mp-left box-style'>
+                        <article className='user-info'>
+                            <span>
+                                <img src="/images/ddung.jpg" alt="" />
+                            </span>
+                            <div>
+                                <p className='f30'><b>홍길동</b>님</p>
+                                    <ul>
+                                        <li className='thin300'>2999.01.01</li>
+                                        <li className='thin300'>jinair@google.com</li>
+                                        <li className='thin300'>010.0000.0000</li>
+                                    </ul>
+                                    <span className='f12 thin300 modify-info'>회원정보수정</span>
+                            </div>
+                        </article>
+                        <article className='user-interest'>
+                            <div>
+
+                            </div>
+                        </article>
+                    </section>
+                    <section className='mp-center'></section>
+                    <section className='mp-right'></section>
+                </div>
+
+            </div>
         </div>
     );
 }
