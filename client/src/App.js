771845b51801fd27534f85a187a18062a5f2df09
2eo22 <2eo2yeo@gmail.com>
2025-03-28 12:32:52 +0900

    mypage, app.js 업뎃

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './scss/common.scss';
 import Layout from './pages/Layout.jsx';
 import Home from './pages/Home.jsx';
 import Mypage from './pages/Mypage.jsx';
+import MyReservation from './pages/MyReservation.jsx';
 
 export default function App() {
   return (
@@ -10,7 +11,8 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path='/mypage' element={<Mypage />} />
+        <Route path='/mypage/index' element={<Mypage />} />
+        <Route path='/mypage/getReservation' element={<MyReservation />} />
         {/* <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/payment' element={<Payment />} />
