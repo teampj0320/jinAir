6326c1a613d73db9091c4183b702df6722452547
haon <haoony@naver.com>
2025-04-09 15:21:54 +0900

    d

--- client/src/component/main/chatbot/Schedule.jsx
+++ client/src/component/main/chatbot/Schedule.jsx
@@ -1,52 +1,107 @@
 import React from 'react';
+import { FaCalendarCheck } from "react-icons/fa";
+
 
 export default function Schedule() {
+    const data = [
+        '서울/인천(ICN)',
+        '서울/김포(GMP)',
+        '부산(PUS)',
+        '제주(CJU)',
+        '광주(KWJ)',
+        '군산(KUV)',
+        '대구(TAE)',
+        '무안(MWX)',
+        '사천(HIN)',
+        '울산(USN)',
+        '원주(WJU)',
+        '여수(RSU)',
+        '청주(CJJ)',
+        '포항/경주(KPO)',
+        '도쿄/나리타(NRT)',
+        '오사카/간사이(KIX)',
+        '후쿠오카(FUK)',
+        '기타큐슈(KKJ)',
+        '나고야(NGO)',
+        '다카마쓰(TAK)',
+        '미야코지마/시모지시마(SHI)',
+        '이시가키지마(ISG)',
+        '삿포로(CTS)',
+        '오키나와(OKA)',
+        '상하이/푸둥(PVG)',
+        '시안(XIY)',
+        '정저우(CGO)',
+        '홍콩(HKG)',
+        '마카오(MFM)',
+        '타이베이/타오위안(TPE)',
+        '타이중(RMQ)',
+        '방콕(BKK)',
+        '푸껫(HKT)',
+        '치앙마이(CNX)',
+        '세부(CEB)',
+        '클락(CRK)',
+        '보홀(TAG)',
+        '다낭(DAD)',
+        '나트랑(CXR)',
+        '푸꾸옥(PQC)',
+        '비엔티안(VTE)',
+        '코타키나발루(BKI)',
+        '괌(GUM)',
+        '울란바토르(UBN)'
+    ];
+    const processedList = data.map(item => item.split("(")[0]);
+        
     return (
-        <div>
-            <div>
+        <div className='schedule-all-box'>
+            <div className='schedule-top-box'>
                 <p>진에어 항공편의 스케줄 조회를 도와드릴게요~</p>
                 <p>출발지, 도착지와 가는날을 선택해주세요!</p>
             </div>
-            <div>
+            <div className='schedule-country-check-box'>
                 <span>출발/도착</span>
                 <select name="" id="">
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
+                    {processedList.map((data) => (
+                        <option value="">{data}</option>
+                    ))}
                 </select>
                 <select name="" id="">
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
+                    {processedList.map((data) => (
+                        <option value="">{data}</option>
+                    ))}
                 </select>
             </div>
-            <div>
+            <div className='schedule-calendar-check-box'>
                 <span>가는날</span>
-                <span>캘린더</span>
-            </div>
-            <button>확인</button>
-            // 확인 클릭되면
-            <div>
-                [출발일:2025-04-12, 출발지:ICN, 도착지:BKK] 스케줄 정보를 조회 중입니다.
-
-                잠시만 기다려 주세요~!!
-            </div>
-            <div>
-                <h5>비행기번호?</h5>
                 <div>
-                    <ul>
-                        <li>출발지<span>출발시간</span></li>
-                        <li>비행기아이콘<span>비행시간</span></li>
-                        <li>도착지<span>도착시간</span></li>
-                    </ul>
-                    <p>*화면에 표시되는 시각은 현지 시각 기준입니다.</p>
+                    <input type="text" />
+                    <FaCalendarCheck />
                 </div>
-                <button>예약하기</button>
-                <button>출도착지 바꾸기</button>
-                // 여기서 예약하기 누르면 그냥 메인페이지로 넘어감 ( 이거 좀 바꿀필요가잇다고 생각)
-                // 출도착지 변경은 걍 두개 변경해주면댐
             </div>
+            <div className='schedule-button-box'>
+                <button>확인</button>
+            </div>            
         </div>
     );
 }
 
+// 확인 클릭되면
+{/* <div>
+[출발일:2025-04-12, 출발지:ICN, 도착지:BKK] 스케줄 정보를 조회 중입니다.
+
+잠시만 기다려 주세요~!!
+</div>
+<div>
+<h5>비행기번호?</h5>
+<div>
+    <ul>
+        <li>출발지<span>출발시간</span></li>
+        <li>비행기아이콘<span>비행시간</span></li>
+        <li>도착지<span>도착시간</span></li>
+    </ul>
+    <p>*화면에 표시되는 시각은 현지 시각 기준입니다.</p>
+</div>
+<button>예약하기</button>
+<button>출도착지 바꾸기</button>
+// 여기서 예약하기 누르면 그냥 메인페이지로 넘어감 ( 이거 좀 바꿀필요가잇다고 생각)
+// 출도착지 변경은 걍 두개 변경해주면댐
+</div> */}
